d9f2fd16e8909195e53cf3b75b100ba38dd031a4
Garent Ecklesia <garentecklesia45678@gmail.com>
2025-12-09 13:00:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,11 @@ def predict_ingredients(image, model, label_encoder, img_size=224, top_k=5):
 # RECIPE GENERATION
 # ---------------------------
 def generate_recipe(ingredients, confidences, user_instruction=""):
-    api_key = os.getenv("API_KEY")
+    
+    if "OPENAI_API_KEY" in st.secrets:
+        api_key = st.secrets["OPENAI_API_KEY"]
+    else:
+        api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
         return "❌ Missing GROQ_API_KEY in .env file."
 
